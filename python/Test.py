ed014cbc5fb053dc2dfd61461051f16a6bc9f81c
devilmhzx <241681095@qq.com>
2019-08-17 02:22:10 -0700

    第八天早上练习

--- python/Test.py
+++ python/Test.py
@@ -1,17 +1,10 @@
-print('万但了！')
-
-a = 12
-
-print(a)
-
-
-
-
-
-
-
-
-
-
+# b = set()
+# while True:
+#     a = int(input('请输入多个整数：'))
+#     if a < 0:
+#         break
+#     b.add(a)
+# print('输入的不同整数数量为：%d' % len(b))
+# print('剩下的输入的整数和为：%d' % sum(list(b)))
 
 
