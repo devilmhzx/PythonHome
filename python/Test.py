b9d3f90383943cd4da23796cdf689289cee0d108
devilmhzx <241681095@qq.com>
2019-08-17 05:25:27 -0700

    第八天下午练习

--- python/Test.py
+++ python/Test.py
@@ -7,15 +7,24 @@
 # print('输入的不同整数数量为：%d' % len(b))
 # print('剩下的输入的整数和为：%d' % sum(list(b)))
 
+# def print_even(n):
+#     # for i in range(0, n):
+#     #     if i % 2 == 0:
+#     #         print(i, end=' ')
+#     for i in range(0, n, 2):
+#         print(i, end=' ')
+#
+#
+# a = int(input('请输入一个整数：'))
+#
+# print_even(a)
 
-def print_even(n):
-    # for i in range(0, n):
-    #     if i % 2 == 0:
-    #         print(i, end=' ')
-    for i in range(0, n, 2):
-        print(i, end=' ')
+list1 = [0, 1, 2, 3]
+list2 = ['libi', 'bilii', 'liwei', 'aini']
+#双巡回推导式
+dict_list = {x: y for x in list1 for y in list2}
+print(dict_list)
+#{0: 'aini', 1: 'aini', 2: 'aini', 3: 'aini'}
 
 
-a = int(input('请输入一个整数：'))
 
-print_even(a)
